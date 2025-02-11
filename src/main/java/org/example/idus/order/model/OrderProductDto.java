236a5d5098ee2551341b1931cc9c9fbe093b81df
sangmi Park <miyad927@naver.com>
2025-02-11 19:24:39 +0900

    [feature] order : 단일 회원의 주문목록 조회 기능 추가

--- src/main/java/org/example/idus/order/model/OrderProductDto.java
+++ src/main/java/org/example/idus/order/model/OrderProductDto.java
@@ -1,9 +1,11 @@
 package org.example.idus.order.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.idus.member.model.Member;
 
 import java.sql.Date;
+import java.util.List;
 
 public class OrderProductDto {
     @Getter
@@ -23,6 +25,37 @@ public class OrderProductDto {
                     .orderDate(orderDate)
                     .member(member)
                     .build();
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ListResponse {
+        private Boolean isSuccess;
+        private List<OrderResponse> result;
+
+        public static ListResponse success(List<OrderResponse> result) {
+            return new ListResponse(true, result);
+        }
+    }
+
+    @Getter
+    public static class OrderResponse {
+
+        private Long idx;
+        private String orderNumber;
+        private String name;
+        private Date orderDate;
+        private Long memberIdx;
+
+        public static OrderResponse from(OrderProduct order) {
+            OrderResponse response = new OrderResponse();
+            response.idx = order.getIdx();
+            response.orderNumber = order.getOrderNumber();
+            response.name = order.getName();
+            response.orderDate = order.getOrderDate();
+            response.memberIdx = order.getMember().getIdx();
+            return response;
         }
     }
 
