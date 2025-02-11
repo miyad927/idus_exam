cfc8d72e16d626672f6b97e03c362117bd8e206d
sangmi Park <miyad927@naver.com>
2025-02-11 17:10:04 +0900

    [feature] Order - 주문번호 추가

--- src/main/java/org/example/idus/order/OrderProductService.java
+++ src/main/java/org/example/idus/order/OrderProductService.java
@@ -7,6 +7,10 @@ import org.example.idus.order.model.OrderProductDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 @RequiredArgsConstructor
 @Service
 public class OrderProductService {
@@ -14,6 +18,31 @@ public class OrderProductService {
 
     @Transactional
     public void register(OrderProductDto.OrderRegister dto, Member member) {
+
+        dto.setOrderNumber(randomNumber("ORDER"));
+
         OrderProduct orderProduct = orderProductRepository.save(dto.toEntity(member));
+    }
+
+    public static String randomNumber(String prefix) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String formattedDateTime = now.format(formatter);
+        String randomString = generateRandomString(5);
+
+        return prefix + formattedDateTime + randomString;
+    }
+
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
     }
 }
