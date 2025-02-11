422216284c4d75ce388b329b55f7bcf62c50a68c
sangmi Park <miyad927@naver.com>
2025-02-11 10:48:06 +0900

    first commit

---
+++ src/test/java/org/example/idus/IdusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.idus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
